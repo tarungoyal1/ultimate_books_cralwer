0140ada41835c5993dff3aafc15efc20164f19a5
Tarun <tarun13317@gmail.com>
2020-06-27 16:15:13 +0530

    fetch_book_details partition error fixes

--- amazonindiabooks/amazonindiabooks/spiders/fetch_book_details.py
+++ amazonindiabooks/amazonindiabooks/spiders/fetch_book_details.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 class FetchBookDetailsSpider(scrapy.Spider):
     name = 'fetch_book_details'
     allowed_domains = ['www.amazon.com']
-    start_urls = ['https://www.amazon.com/Elements-Programming-Interviews-Python-Insiders/dp/1537713949']
+    # start_urls = ['https://www.amazon.com/Elements-Programming-Interviews-Python-Insiders/dp/1537713949']
 
     # Bind this spider with it's own separate pipeline (BookUrlPipeline)
     custom_settings = {
@@ -16,7 +16,7 @@ class FetchBookDetailsSpider(scrapy.Spider):
         }
     }
 
-    batch_size = 10
+    batch_size = 500
 
     def get_urls(self):
         url_list = []
@@ -65,44 +65,47 @@ class FetchBookDetailsSpider(scrapy.Spider):
     def parse(self, response):
         book = {}
 
-        try:
+        book['book_title'] = response.xpath('normalize-space(//h1/span[1]/text())').get()
+        book['book_subtitle'] = response.xpath('normalize-space(//h1/span[2]/text())').get()
 
-            book['book_title'] = response.xpath('normalize-space(//h1/span[1]/text())').get()
-            book['book_subtitle'] = response.xpath('normalize-space(//h1/span[2]/text())').get()
+        authors_data = []
 
-            authors_data = []
+        pattern_ref = re.compile(r'(.*)\/(ref=.*)?')
 
-            pattern_ref = re.compile(r'(.*)\/(ref=.*)?')
+        authors = response.xpath("//div[@id='bylineInfo']//span[contains(@class, 'author')]")
+        for a in authors:
+            author = {}
+            author['author_name'] = a.xpath(".//span[@class='a-declarative']/a/text()").get()
+            author['author_link'] = self.clean_url(
+                response.urljoin(a.xpath(".//span[@class='a-declarative']/a/@href").get()), pattern_ref)
+            authors_data.append(author)
 
-            authors = response.xpath("//div[@id='bylineInfo']//span[contains(@class, 'author')]")
-            for a in authors:
-                author = {}
-                author['author_name'] = a.xpath(".//span[@class='a-declarative']/a/text()").get()
-                author['author_link']  = self.clean_url(response.urljoin(a.xpath(".//span[@class='a-declarative']/a/@href").get()), pattern_ref)
-                authors_data.append(author)
+        book['authors_data'] = authors_data
 
-            book['authors_data'] = authors_data
+        book_rating_avg = response.xpath("//div[@id='averageCustomerReviews']//span[contains(@class, 'a-icon-alt')]/text()").get()
+        if book_rating_avg:
+            book['book_rating_average'] = book_rating_avg.partition('out')[0].rstrip()
 
-            book['book_rating_average'] = response.xpath("//div[@id='averageCustomerReviews']//span[contains(@class, 'a-icon-alt')]/text()").get().partition('out')[0].rstrip()
-            book['book_rating_count'] = response.xpath("//a[@id='acrCustomerReviewLink']/span/text()").get().partition('rating')[0].rstrip()
+        book_rating_count= response.xpath("//a[@id='acrCustomerReviewLink']/span/text()").get()
+        if book_rating_count:
+            book['book_rating_count'] = book_rating_count.partition('rating')[0].rstrip()
 
             # is a bestseller, this will help us scrape futher for bestseller list
-            isBestseller = response.xpath("//div[@class='badge-wrapper']")
+        isBestseller = response.xpath("//div[@class='badge-wrapper']")
 
-            book['bestseller_data'] = {}
+        book['bestseller_data'] = {}
 
-            if isBestseller:
-                book['bestseller_data']["isBestSeller"] = "yes"
-                for data in isBestseller:
-                    book['bestseller_data']["bestseller_link"]      = self.clean_url(response.urljoin(data.xpath(".//a/@href").get()), pattern_ref)
-                    book['bestseller_data']["bestseller_category"]  = data.xpath(".//a/@title").get()
-                    book['bestseller_data']["bestseller_number"]    = data.xpath(".//a/i/text()").get()
-            else:
-                book['bestseller_data']["isBestSeller"] = "no"
+        if isBestseller:
+            book['bestseller_data']["isBestSeller"] = "yes"
+            for data in isBestseller:
+                book['bestseller_data']["bestseller_link"] = self.clean_url(
+                    response.urljoin(data.xpath(".//a/@href").get()), pattern_ref)
+                book['bestseller_data']["bestseller_category"] = data.xpath(".//a/@title").get()
+                book['bestseller_data']["bestseller_number"] = data.xpath(".//a/i/text()").get()
+        else:
+            book['bestseller_data']["isBestSeller"] = "no"
 
-            book["book_url"] = response.request.url
-        except Exception as e:
-            print(str(e))
+        book["book_url"] = response.request.url
         yield book
 
     def clean_url(self, url_string, pattern):
--- amazonindiabooks/amazonindiabooks/spiders/fetch_book_urls.py
+++ amazonindiabooks/amazonindiabooks/spiders/fetch_book_urls.py
@@ -6,9 +6,9 @@ import re
 class FetchBookUrlsSpider(scrapy.Spider):
     name = 'fetch_book_urls'
     allowed_domains = ['www.amazon.com']
-    start_urls = [
-        'https://www.amazon.com/s?i=stripbooks&bbn=5&rh=n%3A283155%2Cn%3A5%2Cn%3A4053%2Cp_n_feature_two_browse-bin%3A10806566011&dc&fst=as%3Aoff&qid=1593244740&rnid=5&ref=sr_nr_n_17',
-    ]
+    # start_urls = [
+    #     'https://www.amazon.com/s?i=stripbooks&bbn=5&rh=n%3A283155%2Cn%3A5%2Cn%3A4053%2Cp_n_feature_two_browse-bin%3A10806566011&dc&fst=as%3Aoff&qid=1593244740&rnid=5&ref=sr_nr_n_17',
+    # ]
     # start_urls = ['https://www.amazon.in/s?rh=n%3A976389031%2Cn%3A%21976390031%2Cn%3A1318158031&page=2&qid=1593170560&ref=lp_1318158031_pg_2']
     # start_urls = ['https://www.amazon.in/s/ref=lp_976389031_nr_n_0?fst=as%3Aoff&rh=n%3A976389031%2Cn%3A%21976390031%2Cn%3A1318158031&bbn=976390031&ie=UTF8&qid=1593169718&rnid=976390031']
 
